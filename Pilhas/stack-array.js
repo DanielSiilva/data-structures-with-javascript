d351f98adbf9f740ed67c59637f23f769eac352b
DanielSiilva <daniellsousa0614@gmail.com>
2024-03-11 17:26:01 -0300

    feat: adcionando pilha com array

---
+++ Pilhas/stack-array.js
@@ -0,0 +1,35 @@
+//Pilha baseada em array
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+  //Add element, sempre no topo da pilha, ou seja no final!
+  push(element) {
+    this.items.push(element);
+  }
+
+  //Remove o elemento da pilha
+  pop() {
+    return this.items.pop();
+  }
+
+  //Devolve o elemento que esta no topo da pilha
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  //Verifica se a pilha esta vazia
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  //Verifica o tamanho da pilha
+  size() {
+    return this.items.length;
+  }
+
+  clear() {
+    this.items = [];
+  }
+}
