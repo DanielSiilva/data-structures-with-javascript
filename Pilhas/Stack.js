327ceede5e318d82f8c9891927e76111bfacc6ea
DanielSiilva <daniellsousa0614@gmail.com>
2024-03-20 18:16:24 -0300

    feat: conceito de pilha refinado

---
+++ Pilhas/Stack.js
@@ -0,0 +1,43 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  //Adiciona um item ao topo da pilha.
+  push(item) {
+    this.items.push(item);
+  }
+
+  //Remove e retorna o item do topo da pilha. Lança um erro se a pilha estiver vazia
+  pop() {
+    if (this.isEmpty()) {
+      throw new Error("Stack is empty");
+    }
+
+    return this.items.pop();
+  }
+
+  //retorna o item do topo da pilha sem removê-lo. Lança um erro se a pilha estiver vazia.
+  top() {
+    if (this.isEmpty()) {
+      throw new Error("Stack is empty");
+    }
+
+    return this.items[this.items.length - 1];
+  }
+
+  //verifica se a pilha está vazia
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  //Retorna o número de itens na pilha.
+  size() {
+    return this.items.length;
+  }
+
+  //Remove todos os itens da pilha.
+  clear() {
+    this.items = [];
+  }
+}
